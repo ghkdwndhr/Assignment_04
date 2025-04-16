e9c66c3c7a19f0d4dbab34578eda10604d27063b
ghkdwndhr <152848677+ghkdwndhr@users.noreply.github.com>
2025-04-16 11:46:56 +0900

    Update count8.py

--- count8_py/count8.py
+++ count8_py/count8.py
@@ -1,3 +1,4 @@
-from gpiozero import LED; from time import sleep
+from gpiozero import LED; 
+from time import sleep
 l=[LED(i) for i in [8,7,16]]
 while 1: [([x.off() for x in l],[l[j].on() for j in range(3) if i>>2-j&1],sleep(1)) for i in range(8)]
